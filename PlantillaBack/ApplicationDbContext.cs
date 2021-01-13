eb85ff6a15e508740a1a9638a7b32536fccc46e6
marcosbustamantemateo <marcosbustamantemateo@gmail.com>
2021-01-13 17:02:06 +0100

    Add files via upload

---
+++ PlantillaBack/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using PlantillaBack.Models;
+
+namespace PlantillaBack
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+    }
+}
---
+++ PlantillaBack/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PlantillaBack.WebApiApplication" Language="C#" %>
